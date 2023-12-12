c8afb730dac543f66eea11816332b84a828703a5
emilo851 <emilo851@student.liu.se>
2023-12-12 13:29:53 +0100

    Big push, some search implemented

--- modules/py_unit.py
+++ modules/py_unit.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING, Any
 
+
 if TYPE_CHECKING:
     from tasks.task import Task
     from library import Unit
@@ -20,9 +21,10 @@ class PyUnit:
         self.task: Task = Idle()
         self.groups = set()
         self.agent: BasicAgent = agent
-        self.max_weapon_cooldown = 0
+        self.max_weapon_cooldown = PyUnit.unit_cooldowns.get(unit.unit_type.name)
         self._last_weapon_cooldown = 0
         self.hp = unit.hit_points
+        self.kite = False
 
     def __repr__(self):
         return f"<PyUnit: {self.unit_type.name}, {self.id}>"
@@ -68,3 +70,27 @@ class PyUnit:
     def remove_group(self, key: Any) -> None:
         """Removes a group from unit's groups"""
         self.groups.remove(key)
+
+    def clone(self):
+        return PyUnit(self.unit, self.agent)
+
+    #List of all cooldown for each units. Used in init for each pyunit.
+    unit_cooldowns = {
+        'TERRAN_MARINE': 0.86,
+        "TERRAN_SIEGETANK": 3.04,
+        "PROTOSS_ZEALOT": 0.86,
+        "ZERG_ZERGLING": 0.696,
+        "PROTOSS_STALKER": 1.44,
+        "ZERG_ROACH": 2.2,
+        "TERRAN_HELLION": 1.43,
+        "TERRAN_HELLBAT": 1.43,
+        "PROTOSS_IMMORTAL": 1.45,
+        "PROTOSS_VOIDRAY": 0.36,
+        "PROTOSS_COLOSSUS": 2.21,
+        "PROTOSS_HIGHTEMPLAR": 0.001,  # High Templar's Feedback has no cooldown
+        "TERRAN_REAPER": 1.5,
+        "TERRAN_BATTLECRUISER": 0.23,
+        "ZERG_MUTALISK": 0.56,
+        "ZERG_ULTRALISK": 0.497,
+        "TERRAN_BANSHEE": 0.8
+    }
--- tasks/attack_scripts.py
+++ tasks/attack_scripts.py
@@ -1,9 +1,13 @@
 from math import sqrt
 from modules.py_unit import PyUnit
-from library import PLAYER_ENEMY, Point2D
+from library import PLAYER_ENEMY, Point2D, PLAYER_SELF
 
 
 class AttackScripts:
+
+
+
+
     @staticmethod
     def attack_nearest(py_unit: PyUnit, agent):
         """Attack the nearest enemy units."""
@@ -17,6 +21,7 @@ class AttackScripts:
     @staticmethod
     def kiting(py_unit: PyUnit, agent):
         """Like attack_nearest but also keep the longest possible distance to enemy while weapon cooldown."""
+        #i start attack kallas move, så efter kite kallar move och dödadr så stannar uniten
         AttackScripts.general_loop("kiting", py_unit, agent)
 
     @staticmethod
@@ -25,13 +30,14 @@ class AttackScripts:
         AttackScripts.general_loop("attack_strongest", py_unit, agent)
 
     @staticmethod
-    def nok_av(py_unit: PyUnit, agent):
+    def nok_av(py_unit: PyUnit, agent, simulation: bool):
         """Like attack_strongest but will NOT attack an enemy that have been dealt lethal attack on current step."""
         pos = py_unit.position
         attack_range = py_unit.unit_type.attack_range
         unit_list = agent.unit_collection.get_group(PLAYER_ENEMY, lambda u: u.unit_type.is_combat_unit)
         strongest_attack = 0
         attack_unit = None
+        py_unit.hp -= 10
 
         for pyunit in unit_list:
             if pos.distance(pyunit.position) <= attack_range:
@@ -40,8 +46,12 @@ class AttackScripts:
                     strongest_attack = pyunit.unit_type.attack_damage
 
         if attack_unit:
-            py_unit.unit.attack_unit(attack_unit.unit)
-            attack_unit.hp = attack_unit.hp - py_unit.unit_type.attack_damage
+            if simulation:
+                attack_unit.hp = attack_unit.hp - py_unit.unit.attack_damage
+                py_unit.hp = 10
+            else:
+                py_unit.unit.attack_unit(attack_unit.unit)
+                attack_unit.hp = attack_unit.hp - py_unit.unit_type.attack_damage
 
     @staticmethod
     def general_loop(script, py_unit, agent):
@@ -80,6 +90,7 @@ class AttackScripts:
                         if py_unit.unit.weapon_cooldown != 0:
                             kite_coords = AttackScripts.calculate_kite(py_unit, pyunit)
                             py_unit.move(kite_coords)
+                            py_unit.kite = True
                             attack_unit = None
 
                 elif script == "attack_strongest":
@@ -94,7 +105,10 @@ class AttackScripts:
     def calculate_kite(py_unit: PyUnit, pyunit: PyUnit):
         """Returns an appropriate position that a given unit should kite to."""
         pos = py_unit.position
-        attack_range = py_unit.unit_type.attack_range
+        if not isinstance(py_unit, PyUnit):
+            attack_range = py_unit.attack_range
+        else:
+            attack_range = py_unit.unit_type.attack_range
         delta_x = pos.x - pyunit.position.x
         delta_y = pos.y - pyunit.position.y
         # Calculate the distance between the points
@@ -109,5 +123,136 @@ class AttackScripts:
         new_y = pyunit.position.y + unit_delta_y * (attack_range + 1)
         return Point2D(new_x, new_y)
 
+    @staticmethod
+    def simulate(state, script):
+     #   if script == "nok_av" and state.player == PLAYER_ENEMY:
+      #      AttackScripts.nok_av_sim_enemy(state.main_unitx, state)
+
+       # if script == "kiting" and state.player == PLAYER_ENEMY:
+        #    AttackScripts.kiting_sim_enemy(state.main_unitx, state)
+
+        if script == "nok_av" and state.player == PLAYER_SELF:
+            AttackScripts.nok_av_sim(state.main_unitx, state)
+
+        if script == "kiting" and state.player == PLAYER_SELF:
+            AttackScripts.kiting_sim(state.main_unitx, state)
+
+
+    @staticmethod
+    def nok_av_sim(unitx, state):
+        """Like attack_strongest but will NOT attack an enemy that have been dealt lethal attack on current step."""
+        pos = unitx.position
+        attack_range = unitx.attack_range
+        unit_list = state.U2
+        strongest_attack = 0
+        attack_unit = None
+
+        for unitx in unit_list:
+            if pos.distance(unitx.position) <= attack_range:
+                if unitx.attack_damage > strongest_attack and unitx.hp > 0:
+                    attack_unit = unitx
+                    strongest_attack = unitx.attack_damage
+
+        if attack_unit:
+            attack_unit.hp = attack_unit.hp - unitx.attack_damage
+
+
+    @staticmethod
+    def kiting_sim(unitx, state):
+        """Like attack_nearest but also keep the longest possible distance to enemy while weapon cooldown."""
+        unitx.hp += 1000
+        pos = unitx.position
+        attack_range = unitx.attack_range
+        unit_list = state.U2
+        nearest_distance = 999999
+        for pyunit in unit_list:
+            if pos.distance(pyunit.position) <= attack_range:
+                if pos.distance(pyunit.position) < attack_range \
+                        and pos.distance(pyunit.position) < nearest_distance:
+                    nearest_distance = pos.distance(pyunit.position)
+
+                    kite_coords = AttackScripts.calculate_kite(unitx, pyunit)
+                    unitx.position = kite_coords
+
+
+    @staticmethod
+    def nok_av_sim_enemy1(myunit, state):
+        """Like attack_strongest but will NOT attack an enemy that have been dealt lethal attack on current step."""
+        pass
+        pos = myunit.position
+        unit_list = state.U2
+        strongest_attack = 0
+        attack_unit = None
+
+
+        for unitx in unit_list:
+            attack_range = unitx.attack_range
+            if pos.distance(unitx.position) <= attack_range:
+                if unitx.attack_damage > strongest_attack : #and unitx.weapon_cooldown == 0:
+                    attack_unit = unitx
+                    strongest_attack = unitx.attack_damage
+
+        if attack_unit:
+            myunit.hp = myunit.hp - attack_unit.attack_damage
+            return True
+        return False
+
+    @staticmethod
+    def kiting_sim_enemy(myunit, state):
+        """Like attack_nearest but also keep the longest possible distance to enemy while weapon cooldown."""
+        pass
+        pos = myunit.position
+        unit_list = state.U2
+        nearest_distance = 999999
+        for pyunit in unit_list:
+            attack_range = pyunit.attack_range
+            if pos.distance(pyunit.position) < attack_range \
+                    and pos.distance(pyunit.position) < nearest_distance:
+                nearest_distance = pos.distance(pyunit.position)
+
+                kite_coords = AttackScripts.calculate_kite(pyunit, myunit)
+                pyunit.position = kite_coords
+
     # Function to calculate the distance between two points on the map
     Point2D.distance = lambda self, other: sqrt((self.x - other.x) ** 2 + (self.y - other.y) ** 2)
+
+    @staticmethod
+    def nok_av_simTEST(unitx, state):
+        """Like attack_strongest but will NOT attack an enemy that have been dealt lethal attack on current step."""
+        pos = unitx.position
+        attack_range = unitx.attack_range
+        unit_list = state.U2
+        strongest_attack = 0
+        attack_unit = None
+
+        for unitx in unit_list:
+            if pos.distance(unitx.position) <= attack_range:
+                if unitx.attack_damage > strongest_attack and unitx.hp > 0:
+                    attack_unit = unitx
+                    strongest_attack = unitx.attack_damage
+
+        if attack_unit:
+            attack_unit.hp = attack_unit.hp - unitx.attack_damage
+            return True
+        return False
+
+    @staticmethod
+    def nok_av_sim_enemy(myunit, state):
+        """Like attack_strongest but will NOT attack an enemy that have been dealt lethal attack on current step."""
+        pass
+        pos = myunit.position
+        unit_list = state.U2
+        strongest_attack = 0
+        attack_unit = None
+
+        for unitx in unit_list:
+            attack_range = unitx.attack_range
+            if pos.distance(unitx.position) <= attack_range:
+                if unitx.attack_damage > strongest_attack:  # and unitx.weapon_cooldown == 0:
+                    attack_unit = unitx
+                    strongest_attack = unitx.attack_damage
+
+        if attack_unit:
+            myunit.hp = myunit.hp - attack_unit.attack_damage
+            return True
+        return False
