df072329ce7e53d2919b3448a509dd8b9dd880c4
vinga129 <vinga129@student.liu.se>
2023-12-12 14:12:28 +0100

    updated lower/raise function

--- agents/basic_agent.py
+++ agents/basic_agent.py
@@ -76,17 +76,16 @@ class BasicAgent(pycc.IDABot):
             for tile in bott:
                 self.map_tools.draw_tile(tile, pycc.Color.BLUE)
 
-        return_set = set(self.unit_collection.py_units.values())
-        all_supply_depots = {py_unit for py_unit in return_set if py_unit.unit_type.unit_typeid == UNIT_TYPEID.TERRAN_SUPPLYDEPOT}
-        all_firendly_units = {py_unit for py_unit in return_set if py_unit.unit_type.is_worker}
+        all_friendly = set(u for u in self.unit_collection.py_units.values() if u.player == pycc.PLAYER_SELF)
+        all_supply_depots = {py_unit for py_unit in all_friendly if py_unit.unit_type.unit_typeid == UNIT_TYPEID.TERRAN_SUPPLYDEPOT or py_unit.unit_type.unit_typeid == UNIT_TYPEID.TERRAN_SUPPLYDEPOTLOWERED}
+        all_friendly_units = {py_unit for py_unit in all_friendly if py_unit.unit_type.is_worker or py_unit.unit_type.is_combat_unit}
         for supply_depot in all_supply_depots:
-            # Juat lowers all at the moment
-            supply_depot.ability(ABILITY_ID.MORPH_SUPPLYDEPOT_RAISE)
-            is_worker_nearby = any(bottle.distance_between_tiles(supply_depot.tile_position, unit.tile_position) < 2 
-                                   for unit in all_firendly_units)
-            if is_worker_nearby:
+            is_friendly_unit_nearby = any(bottle.distance_between_tiles(supply_depot.tile_position, unit.tile_position) < 3
+                                   for unit in all_friendly_units)
+            if is_friendly_unit_nearby:
                 supply_depot.ability(ABILITY_ID.MORPH_SUPPLYDEPOT_LOWER)
-
+            else:
+                supply_depot.ability(ABILITY_ID.MORPH_SUPPLYDEPOT_RAISE)
 
         if self.current_frame % FRAME_SKIP == 1:
             if DEBUG_LOGS:
